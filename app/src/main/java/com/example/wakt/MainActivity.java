db9fab195ff563d3297ea7cf569e78a829acf8a7
Hp <mushfiqhossain22@gmail.com>
2022-11-11 09:30:37 +0600

    Wakt app

--- app/src/main/java/com/example/wakt/MainActivity.java
+++ app/src/main/java/com/example/wakt/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.wakt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView lefticon= findViewById(R.id.left_icon);
+        ImageView righticon= findViewById(R.id.right_icon);
+        //TextView title= findViewById(R.id.toolbar_title);
     }
 }
